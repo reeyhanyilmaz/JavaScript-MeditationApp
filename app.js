db6e255b7b27389b3697a492853f364e591bbf22
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-03-13 10:45:53 +0300

    a few changes

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ sounds.forEach(sound => {
     sound.addEventListener("click", function(){ 
     song.src = this.getAttribute("data-sound");
     video.src = this.getAttribute("data-video");
-    video.setAttribute('playsinline', 'playsinline');
+    video.setAttribute('playsinline', 'playsinline'); //safari'de videonun inline oynatmasi için.
     checkPlaying(song); // song ve video getir click olunca değiştirme func. çalıştır.  
     });
 });
